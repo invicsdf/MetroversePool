10d403a4f060fad9d2891be443287b9c4c9e0671
invicsdf <florent.dupont4c0@gmail.com>
2022-06-09 01:56:02 -0400

    correction bug fonctionnement, captations erreurs et ajouts de console d'erreurs

--- TutoBlockListingV2.js
+++ TutoBlockListingV2.js
@@ -3,135 +3,189 @@ const [bld, par, sdK, clef, until] = [new selenium.Builder(), selenium.By, selen
 const fs= require('fs');
 
 
-async function creerDriver(){
+/* async function creerDriver(){
     
-    return bld.forBrowser("firefox").build()
-}
+    return bld.forBrowser("MicrosoftEdge").build()
+} */
 
-function ouvrirPageHtml(){
-    url = process.argv.slice(2)[0]
-    //driver.get('https://opensea.io/collection/metroverse-genesis?search[sortAscending]=true&search[sortBy]=PRICE&search[toggles][0]=BUY_NOW')
-    //driver.get('https://opensea.io/collection/metroverse-blackout?search[sortAscending]=true&search[sortBy]=PRICE&search[toggles][0]=BUY_NOW')
-    driver.get(process.argv.slice(2)[0])
+function ouvrirPageHtml(driver){
+    try {
+            url = process.argv.slice(2)[0]
+            //driver.get('https://opensea.io/collection/metroverse-genesis?search[sortAscending]=true&search[sortBy]=PRICE&search[toggles][0]=BUY_NOW')
+            //driver.get('https://opensea.io/collection/metroverse-blackout?search[sortAscending]=true&search[sortBy]=PRICE&search[toggles][0]=BUY_NOW')
+            driver.get(process.argv.slice(2)[0])
+    } catch(err) {
+            //console.log("erreur dans l'ouverture du driver et de la page: ", err)
+            error = "erreur dans l'ouverture du driver et de la page: \n" + err 
+    }
     //console.log('url', url)
     //console.log(process.argv.slice(2)[1])
 }
 
-async function etendreAffichageBlock()
+async function etendreAffichageBlock(driver)
 { 
     //cacherFiltre = driver.findElement(par.xpath('//*[@id="main"]/div/div/div[3]/div/div/div/div[2]/div/div/header/button/div[2]'))
-    cacherFiltre = driver.findElement(par.xpath('//*[@id="main"]/div/div/div[5]/div/div[3]/div[1]/div/div/div/div/div[1]/div/button/span'))
-    await cacherFiltre.click()
+    try {
+            //cacherFiltre = driver.findElement(par.xpath('//*[@id="main"]/div/div/div[3]/div/div/div/div[2]/div/div/header/button/div[2]'))                  //mauvais
+            cacherFiltre = driver.findElement(par.xpath('//*[@id="main"]/div/div/div[5]/div/div[3]/div[1]/div/div/div/div/div[1]/div/button/span'))
+            await cacherFiltre.click()
+    } catch(err) {
+            //console.log("erreur etendue de l'affichage: ", err)
+            error = "erreur etendue de l'affichage: \n" + err 
+    }
+
+    //console.log("après filtre cacher")
     //display = driver.findElement(par.xpath('//*[@id="main"]/div/div/div[3]/div/div/div/div[3]/div[1]/div[2]/div[4]/div/button[2]/div'))
-    display = driver.findElement(par.xpath('//*[@id="main"]/div/div/div[5]/div/div[3]/div[1]/div/div/div/div/div[4]/div/div/button[2]/div'))
+
+    /* display = driver.findElement(par.xpath('//*[@id="main"]/div/div/div[5]/div/div[3]/div[1]/div/div/div/div/div[4]/div/div/button[2]/div'))
     await display.click()
-    //console.log("après click")
+    console.log("avant augmenter display") */
 }
 
-async function selectionnerBlock(){
+async function selectionnerBlock(driver){
     //return await driver.findElements(par.className('Blockreact__Block-sc-1xf18x6-0 Assetreact__StyledContainer-sc-bnjqwy-0 elqhCm bwCDxg Asset--loaded'))
-    return await driver.findElements(par.className('sc-1xf18x6-0 sc-bnjqwy-0 hDbqle floEcI Asset--loaded'))
+    try {
+            //return await driver.findElements(par.className('Blockreact__Block-sc-1xf18x6-0 Assetreact__StyledContainer-sc-bnjqwy-0 elqhCm bwCDxg Asset--loaded'))        //mauvais
+            return await driver.findElements(par.className('sc-1xf18x6-0 sc-bnjqwy-0 haVRLx floEcI Asset--loaded'))
+            //sc-1xf18x6-0 sc-bnjqwy-0 hDbqle floEcI Asset--loaded                                                
+    } catch(err) {
+            //console.log("erreur selection et recherche blocs listés: ", err)
+            error = "erreur selection et recherche blocs listés: \n" + err
+    }
 }
 
 
-async function scrollDown()
+async function scrollDown(driver)
 {
-    pageBlock = driver.findElement(par.css("body"));
-    await pageBlock.sendKeys(clef.PAGE_DOWN);
-    await driver.sleep(2500);
-    //await driver.wait(until.elementLocated(par.className('AssetCardFooter--name')))
-    //await driver.wait(selectionnerBlock())
-    await driver.wait(async function() {
-        const readyState = await driver.executeScript('return document.readyState')
-        return readyState === 'complete'
-      });
+    try {
+            pageBlock = driver.findElement(par.css("body"));
+            await pageBlock.sendKeys(clef.PAGE_DOWN);
+            await driver.sleep(2500);
+            //await driver.wait(until.elementLocated(par.className('AssetCardFooter--name')))
+            //await driver.wait(selectionnerBlock())
+            await driver.wait(async function() {
+                const readyState = await driver.executeScript('return document.readyState')
+                return readyState === 'complete'
+            });
+        }
+    catch (err) {
+                error = "erreur de scrolling: \n" + err
+        }
+    
 }
 
-async function isBuyNow(block)
+async function isBuyNow(block, driver)
 {
     try {
             await driver.sleep(10);
-            //await block.findElements(par.className('Blockreact__Block-sc-1xf18x6-0 Textreact__Text-sc-1w94ul3-0 cKQpdV dsumKY'))
+            //buyNow = await block.findElements(par.className('Blockreact__Block-sc-1xf18x6-0 Textreact__Text-sc-1w94ul3-0 cKQpdV dsumKY'))   //mauvais
             await block.findElements(par.className('sc-1xf18x6-0 sc-1w94ul3-0 bRplOh crRCcg'))
 
-            return true
+            return true    
     }
     catch (err) {
             //num = await numeroBlock(block)
             //console.log(num)
+            error = "erreur scrapping isBuyNow: \n" + err
             return false
     }
     
 }
 
-async function numeroBlock(block)
+async function numeroBlock(block, driver)
 {
     await driver.sleep(10);
-    await block.findElement(par.className('AssetCardFooter--name')).getText().then(function (num) 
-        {
-            id = num.slice(7)
-        });
-    
-
-    return id
+    try {               
+            //await block.findElement(par.className('AssetCardFooter')).getText().then(function (num)   //mauvais
+            await block.findElement(par.className('AssetCardFooter--name')).getText().then(function (num) 
+            {
+                id = num.slice(7)
+            });
+        
+            return id
+    } catch(err) {
+            //console.log("erreur scrapping numero bloc: ", err)
+            error = "erreur scrapping numero bloc: \n" + err
+    }
 }
 
-async function prixBlock(block, idBlock)
+async function prixBlock(block, idBlock, driver)
 {
     var prixBlk = -1
     await driver.sleep(10);
     //prix = block.findElement(par.className('Overflowreact__OverflowContainer-sc-7qr9y8-0 jPSCbX Price--amount'))
-    prix = block.findElement(par.className('sc-7qr9y8-0 iUvoJs Price--amount'))
-    
-    if (blockListed[idBlock] == undefined)
-    {
-        await prix.getText().then(function (price) 
-        {
-            //console.log(compteur++)
-            blockListed[idBlock] = price
-            prixBlk = price
-        });
-    
-    }
+    try {
+            //prix = block.findElement(par.className('Overflowreact__OverflowContainer-sc-7qr9y8-0 jPSCbX Price--amount'))   //mauvais
+            prix = block.findElement(par.className('sc-7qr9y8-0 iUvoJs Price--amount'))
+            if (blockListed[idBlock] == undefined)
+            {
+                await prix.getText().then(function (price) 
+                {
+                    //console.log(compteur++)
+                    blockListed[idBlock] = price
+                    prixBlk = price
+                });
+            }
 
-    return prixBlk
+            return prixBlk
+    } catch(err) {
+            error = "erreur scrapping prix bloc: \n" + err
+            //console.log("erreur scrapping prix bloc: ", err)
+    }
 }
 
-async function enregistrerInfosBlock()
+async function enregistrerInfosBlock(driver)
 {
     continuer = true
     //await scrollDown()
     while (continuer)
     {
-        await scrollDown()
-        await scrollDown()
-        blocks = await selectionnerBlock()
+        await scrollDown(driver)
+        await scrollDown(driver)
+        blocks = await selectionnerBlock(driver)
+        if (blocks.length == 0)
+        {
+            //console.log("Erreur scrapping: aucun bloc listé")
+            error = "Erreur scrapping: aucun bloc listé scrappé"
+            break
+        }
+        //console.log(blocks)
         continuer = false
-        //console.log(blocks.length)
+        //console.log("Nombre de blocks listés: ", blocks.length)
         for (i = 0; i < blocks.length; i++)
         {
-            if (await isBuyNow(blocks[i]))
+            if (await isBuyNow(blocks[i], driver))
             {
                 //console.log("appel de num block")
-                idBlock = await numeroBlock(blocks[i])
+                idBlock = await numeroBlock(blocks[i], driver)
                 //console.log("appel de prix block")
-                if (await prixBlock(blocks[i], idBlock) > 0)
+                prixBloc = await prixBlock(blocks[i], idBlock, driver)
+                if (prixBloc > 0)
                 {
                     continuer = true
                 }
+                /* else if (prixBloc == -1) 
+                {
+                    break
+                } */
             }
         }
        // console.log("bloc suivant")
     }
+    if (error != 0)
+    {
+        console.log(error)
+    }
 }
 
 //const driver = creerDriver()
-let driver = bld.forBrowser("MicrosoftEdge").build();
 
 compteur = 1
 blockListed = {};
+error = 0;
 
 (async () => {
+    let driver = await bld.forBrowser("firefox").build();
     try {
             // driver.manage().window().setSize(200,200);
             //await driver.manage().window().setRect({x:0, y:0, width:30000, height:100000})
@@ -139,16 +193,26 @@ blockListed = {};
             await driver.manage().window().maximize()
 
             //console.log("creerDriver")
-            ouvrirPageHtml()
+            ouvrirPageHtml(driver)
+            if (error != 0)
+            {
+                console.log(error)
+            }
+
             //console.log("ouvrirPageHtml")
-            await etendreAffichageBlock();
+            await etendreAffichageBlock(driver);
+            if (error != 0)
+            {
+                console.log(error)
+            }            
             //console.log("etendreAffichageBlock")
             //const blocks = selectionnerBlock()
             //console.log("après selectionnerBlock")
             //blocks.then(console.log)
-            await enregistrerInfosBlock()
+            await enregistrerInfosBlock(driver)
             //console.log(blockListed)
             //console.log(Object.keys(blockListed).length )
+
             if (blockListed.hasOwnProperty(''))
             {
                 delete blockListed[""]
@@ -156,13 +220,15 @@ blockListed = {};
             //console.log(blockListed)
             //console.log(Object.keys(blockListed).length)
             const data = JSON.stringify(blockListed);
-            //fs.writeFile(process.argv.slice(2)[1], data, (err) => 
-            fs.writeFile(process.argv.slice(2)[1], data, (err) => 
+            if (error == 0)
             {
-                    if (err) 
-                            console.log('erreur dans le writefile', err);
-                    else    console.log("JSON data is saved.");
+                fs.writeFile(process.argv.slice(2)[1], data, (err) => 
+                {
+                        if (err) 
+                                console.log('erreur dans le writefile', err);
+                        else    console.log("JSON data is saved.");
                 });
+            }
         } finally {
              //await driver.quit();
         }
